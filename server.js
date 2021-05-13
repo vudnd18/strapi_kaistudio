ceebbf60db2ec6078dca9072c28a36c411e4f2d9
vũ <vudnd18@gmail.com>
2021-05-13 11:43:53 +0700

    fix file server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const strapi = require('strapi');
-module.exports = ({ env }) => ({
-  port: env.int('PORT', 1337),
-};
+// module.exports = ({ env }) => ({
+//   port: env.int('PORT', 1337),
+// };
 strapi().start();
